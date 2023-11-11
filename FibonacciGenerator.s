e0ae3b0421d0abfb441891c7ac021cac6a63bc0a
Kryptonite <102353731+biraj-dahal@users.noreply.github.com>
2023-11-11 01:14:10 -0500

    Update FibonacciGenerator.s
    Student Description, and Project.

--- FibonacciGenerator.s
+++ FibonacciGenerator.s
@@ -1,1 +1,6 @@
+# Biraj Dahal 
+# @03070737
+# biraj.dahal@bison.howard.edu
+# Fibonacci Series Project Computer Organization I
+# Code tested, and working on my Computer (M1 Macbook Pro) - Arithmetic Overflow after Fibonancci 46. So to prevent error select N <=46.
 
