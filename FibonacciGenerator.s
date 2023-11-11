2a6d08eb2e3ee2ea92f562fee0fc34fc5164ba18
Kryptonite <102353731+biraj-dahal@users.noreply.github.com>
2023-11-11 01:17:01 -0500

    Added directives, and string to later use in the code.

--- FibonacciGenerator.s
+++ FibonacciGenerator.s
@@ -4,3 +4,9 @@
 # Fibonacci Series Project Computer Organization I
 # Code tested, and working on my Computer (M1 Macbook Pro) - Arithmetic Overflow after Fibonancci 46. So to prevent error select N <=46.
 
+.data
+	askUser : .asciiz "Find fibonacci of number (N>=25). Enter N: " # using it to store input question. through null terminated .ascii
+	invalid : .asciiz "Illegal Number!"  # using it to store output warning. through null terminated .ascii
+	newline: .ascii "\n" #using it to store address of a new line character
+.text
+.globl main
